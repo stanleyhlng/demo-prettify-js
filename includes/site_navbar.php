865fb4e1143c54cb23112a9638a31f077b996885
Stanley Ng <stanleyhlng@yahoo.com.hk>
2012-07-04 19:25:15 +0800

    added dropdown menu

--- includes/site_navbar.php
+++ includes/site_navbar.php
@@ -1,3 +1,24 @@
+<?php
+	include_once 'helpers.php';
+	$pageName = getValue( $_SERVER, 'SCRIPT_NAME' );
+	$pageName = basename( $pageName, ".php" );
+	
+	$id = getValue( $_REQUEST, "id" );
+	
+	$themes = array();
+	$themes['bootstrap'] = array(
+		'name' => 'Bootstrap',
+		'link' => '/prettify-js/theme.php?id=bootstrap'
+		);
+	$themes['tomorrow-night-blue'] = array(
+		'name' => 'Tomorrow Night Blue',
+		'link' => '/prettify-js/theme.php?id=tomorrow-night-blue'
+		);
+	$themes['solarized-dark'] = array(
+		'name' => 'Solarized Dark',
+		'link' => '/prettify-js/theme.php?id=solarized-dark'
+	);
+?>
 <div class="navbar navbar-fixed-top">
 	<div class="navbar-inner">
 		<div class="container">
@@ -18,6 +39,26 @@
 				</ul>
 			</div>
 			//-->
+
+			<ul class="nav pull-right">
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+						<?php echo strtolower( $themes[$id]['name'] ); ?>
+						<b class="caret"></b>
+					</a>
+					<ul class="dropdown-menu">
+						<?php foreach ($themes as $themeId => $theme): ?>
+							<?php
+								if ( $themeId === $id ) 
+								{
+									continue;
+								}
+							?>
+						<li><a href="<?php echo $theme['link']; ?>"><?php echo strtolower( $theme['name'] ); ?></a></li>
+						<?php endforeach; ?>
+					</ul>
+				</li>
+			</ul>			
 		</div>
 	</div>
 </div>
